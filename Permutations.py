d83ea0a4456fd55c3d63dc23d0de4d5c0042d9ad
Joshua Broussard <123220700+BushidoBee@users.noreply.github.com>
2024-01-04 09:41:47 -0600

    Update Permutations.py
    Complete

--- Permutations.py
+++ Permutations.py
@@ -1,15 +1,21 @@
 ### Permutations creates all combinations of list items / iterables
 import itertools
+# Define the Alphabet in a String variable 'Alpha'
+ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 # Create a permutation and set it to a variable; Extra check: If 'string' is all UPPERCASE
 string, numchars = input().split()
 num = int(numchars)
 strperm = sorted(list(itertools.permutations(string, num))) # Note: Slicing the permutation to only 2 characters
-print (strperm)
+# print (strperm)
 
 # Print the permutations in alphabetical order using a loop; remove the selection from the list
 for iteration in range(len(strperm)):
-    iterable = strperm[iteration][0] + strperm[iteration][1]
+    i = 0
+    iterable = ""
+    while i != len(strperm[iteration]):
+        iterable = iterable + strperm[iteration][i] # Create the iteration
+        i+=1
+        # for 
+        # iterable = strperm[iteration][0] + strperm[iteration][1]
     print(iterable) # print the interation
-        
-    
