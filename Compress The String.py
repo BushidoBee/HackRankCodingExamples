366c267d4e6fca67e60d956ac2f98c54924cf29a
Joshua Broussard <123220700+BushidoBee@users.noreply.github.com>
2024-01-04 21:29:25 -0600

    Update Compress The String.py
    Completed on 01/04

--- Compress The String.py
+++ Compress The String.py
@@ -1,15 +1,16 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import itertools
-import collections
 # Will be using "groupby()" function and other associated with the "Itertools" library
 # itertools.groupby(iterable[, key])
 s = input()
-string_one = itertools.groupby(s)
-print (list(string_one[0]))
+groups = []
+string_one = list(itertools.groupby(s))
+# print(list(string_one))
 
 
 # Use a loop, iterate through all of the groups; end it with a space
-
+for key, group_obj in itertools.groupby(s):
+    groups.append("(" + str(len(list(group_obj))) + ", " + str(key) + ")")
 
 # print the results
-
+print(*groups)
