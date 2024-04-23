132486ef23100e4a32d8014a5b6fe9e5d7d60fb8
Joshua Broussard <123220700+BushidoBee@users.noreply.github.com>
2024-04-23 15:56:46 -0500

    Added Intersections Line
    Same Code, swap lines below 1 & 2

--- Coding Solutions/Coding Practice - Python/Sets - Union, Intersections.py
+++ Coding Solutions/Coding Practice - Python/Sets - Union, Intersections.py
@@ -2,3 +2,4 @@ A_num, Set_A = input(), set(map(int, input().split()))
 B_num, Set_B = input(), set(map(int, input().split()))
 
 print(len(Set_A.union(Set_B)))
+print(len(Set_A.intersection(Set_B)))
